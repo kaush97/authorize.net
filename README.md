1c2b20d3015910ec8e25c0a18b9a5aa465bdac54
unknown <kaushal30061997@gmail.com>
2021-08-24 13:57:49 +0530

    authorize

---
+++ README.md
@@ -0,0 +1,11 @@
+# react-authorize-net
+
+This repo contains an example of a React application (bootstrapped using [create-react-app](https://github.com/facebook/create-react-app)) using the [react-authorize-net](https://github.com/j-em/react-authorize-net) library.
+
+## How to run locally
+
+```
+npm run start
+```
+
+The application should then be reachable at http://localhost:8080
---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { Flex, Box, Text, Heading } from "rebass";
+import styled from "styled-components";
+import logo from "./logo.svg";
+import { FormComponent, FormContainer } from "react-authorize-net";
+import R from "ramda";
+
+let clientKey = '2hsv7AdmtQk62xwAMQH675BhWK3vgwT8Xq3MK9RbTx8DEGA5SN5dfv7cW9G3rvwH';
+let apiLoginId = '69ph66RQBp';
+
+type State = {
+  status: "paid" | "unpaid" | ["failure", string[]];
+};
+
+const Button = styled.button({
+  "&:hover": { cursor: "pointer" },
+  padding: "10px",
+  backgroundColor: "white",
+  border: "2px solid black",
+  fontWeight: 600,
+  borderRadius: "2px"
+});
+
+const ErrorComponent = (props: {
+  errors: string[];
+  onBackButtonClick: () => void;
+}) => (
+  <div>
+    <Text fontSize={3} fontWeight={"500"} mb={3}>
+      Failed to process payment
+    </Text>
+    {props.errors.map(error => (
+      <Text py={2}>{error}</Text>
+    ))}
+    <Button onClick={props.onBackButtonClick}>Go Back</Button>
+  </div>
+);
+
+const Header = props => (
+  <Flex py={4}>
+    <Heading>react-authorize-net-example</Heading>
+  </Flex>
+);
+
+class App extends Component<{}, State> {
+  state: State = { status: "unpaid" };
+
+  onErrorHandler = (response: any) => {
+    this.setState({
+      status: ["failure", response.messages.message.map(err => err.text)]
+    });
+  };
+
+  onSuccessHandler = (response: any) => {
+    // Process API response on your backend...
+    this.setState({ status: ["failure", []] });
+  };
+
+  render() {
+    return (
+      <Box className="App" p={3}>
+        <Header />
+        {this.state.status === "paid" ? (
+          <Text fontWeight={"500"} fontSize={3} mb={4}>
+            Thank you for your payment!
+          </Text>
+        ) : this.state.status === "unpaid" ? (
+          <FormContainer
+            environment="sandbox"
+            onError={this.onErrorHandler}
+            onSuccess={this.onSuccessHandler}
+            amount={23}
+            component={FormComponent}
+            clientKey={clientKey}
+            apiLoginId={apiLoginId}
+          />
+        ) : this.state.status[0] === "failure" ? (
+          <ErrorComponent
+            onBackButtonClick={() => this.setState({ status: "unpaid" })}
+            errors={this.state.status[1]}
+          />
+        ) : null}
+      </Box>
+    );
+  }
+}
+
+export default App;
